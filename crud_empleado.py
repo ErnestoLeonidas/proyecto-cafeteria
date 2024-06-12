7da1c7dd073d7f2cc1339f303bcf2803bb0e3d2e
Ernesto Velásquez <34869177+ErnestoLeonidas@users.noreply.github.com>
2024-06-11 21:38:37 -0400

    Version 2

--- crud_empleado.py
+++ crud_empleado.py
@@ -5,7 +5,9 @@ os.system("cls")
 def cargar_json(url_archivo):
     with open(url_archivo, 'r') as archivo:
         return json.load(archivo)
-    
-empleados = cargar_json('empleados.json')
 
-print(empleados)
+def iniciar_programa():
+    empleados = cargar_json('empleados.json')
+    print(empleados)
+
+iniciar_programa()

d6277736f691eeb66c60b9d04b0d3c59782aa74c
Ernesto Velasquez <ernestoleonidas@gmail.com>
2024-06-13 20:12:21 -0400

    s10

--- crud_empleado.py
+++ crud_empleado.py
@@ -1,13 +1,100 @@
 import os
 import json
-os.system("cls")
+os.system("clear")
+
+URL_EMPLEADOS = 'empleados.json'
+# URL_CARGOS
+# URL_PRODUCTOS
 
 def cargar_json(url_archivo):
-    with open(url_archivo, 'r') as archivo:
-        return json.load(archivo)
+    try:
+        with open(url_archivo, 'r') as archivo:
+            return json.load(archivo)
+    except:
+        return []
+
+def guardar_archivo(url_archivo, data):
+    with open(url_archivo, 'w') as archivo:
+        json.dump(data, archivo, indent=4)
+
+def crear_empleado():
+    empleados = cargar_json(URL_EMPLEADOS)
+    
+    id_empleado = input("Ingrese el ID del empleado >> ")
+    nombre = input("Ingrese NOMBRE del empleado >> ")
+    apellido = input("Ingrese APELLIDO del empleado >> ")
+    try:
+        sueldo = int(input("Ingrese el SUELDO del empleado >> "))
+    except:
+        sueldo = 0
+    id_cargo = input("Ingrese el CARGO del empleado >> ")
+    
+    nuevo_empleado = {
+        "id_empleado": id_empleado,
+        "nombre": nombre,
+        "apellido": apellido,
+        "sueldo": sueldo,
+        "id_cargo": id_cargo
+    }
+    
+    empleados.append(nuevo_empleado) # acá tenemos el nuevo empleado añadido solamente al array y no al archivo json
+    # ahora guardamos el array dentro del archivo json
+    guardar_archivo(URL_EMPLEADOS, empleados)
+
+def actualizar_empleado():
+    empleados = cargar_json(URL_EMPLEADOS)
+    
+    id_empleado = input("Ingrese el ID del empleado a modificar >> ")
+    
+    nombre = input("Ingrese NOMBRE del empleado >> ")
+    apellido = input("Ingrese APELLIDO del empleado >> ")
+    try:
+        sueldo = int(input("Ingrese el SUELDO del empleado >> "))
+    except:
+        sueldo = 0
+    id_cargo = input("Ingrese el CARGO del empleado >> ")
+    
+    for emp in empleados:
+        if emp['id_empleado'] == id_empleado:
+            emp['nombre'] = nombre
+            emp['apellido'] = apellido
+            emp['sueldo'] = sueldo
+            emp['id_cargo'] = id_cargo
+            break
+        
+    guardar_archivo(URL_EMPLEADOS, empleados)
+
+def eliminar_empleado():
+    empleados = cargar_json(URL_EMPLEADOS)
+    id_empleado = input("Ingrese el ID del empleado a eliminar >> ")
+    
+    aux_empleado = []
+    
+    for emp in empleados:
+        if emp['id_empleado'] != id_empleado:
+            aux_empleado.append(emp)
+    
+    guardar_archivo(URL_EMPLEADOS, aux_empleado)
+    
+
+def listar_empleados():
+    empleados = cargar_json(URL_EMPLEADOS)
+    
+    headers = ['id_empleado', 'nombre', 'apellido', 'sueldo', 'id_cargo']
+    col_widths = [15,15,15,15,15]
+    
+    # Imprimir el encabezado
+    header_line = ' | '.join(f"{header.capitalize():^{col_widths[i]}}" for i, header in enumerate(headers))
+    print(header_line)
+    print('-' * len(header_line))
+    
+    for empleado in empleados:
+        row = ' | '.join(f"{str(empleado.get(header, '')):^{col_widths[i]}}" for i, header in enumerate(headers))
+        print(row)
+    
 
 def menu_general():
-    os.system("cls")
+    os.system("clear")
     print("1. Crear empleado")
     print("2. Actualizar empleado")
     print("3. Eliminar empleado")
@@ -32,17 +119,17 @@ def iniciar_programa():
         opcion = seleccionar_opcion(5)
 
         if opcion == 1:
-            print("1. Crear empleado")
+            crear_empleado()
         elif opcion == 2:
-            print("2. Actualizar empleado")
+            actualizar_empleado()
         elif opcion == 3:
-            print("3. Eliminar empleado")
+            eliminar_empleado()
         elif opcion == 4:
-            empleados = cargar_json('empleados.json')
-            print(empleados)
+            listar_empleados()
         elif opcion == 5:
             return
 
         input()
-        
-iniciar_programa()
+
+if __name__ == "__main__":
+    iniciar_programa()

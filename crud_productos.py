340d435cb5de6fcac401368cab66606820b1f257
Ernesto Velásquez <34869177+ErnestoLeonidas@users.noreply.github.com>
2024-06-14 20:16:02 -0400

    actualizar

--- crud_productos.py
+++ crud_productos.py
@@ -7,7 +7,7 @@ FIELDNAMES_PRODUCTOS = ['id_producto', 'nombre_producto', 'precio', 'stock']
 
 def leer_archivo_csv(dir):
     try:
-        with open(dir, mode='r', newline='') as archivo:
+        with open(dir, mode='r', newline='', encoding='utf-8') as archivo:
             data = csv.DictReader(archivo)
             return list(data)
     except:
@@ -15,7 +15,7 @@ def leer_archivo_csv(dir):
 
 def guardar_archivo_csv(dir, data, fieldnames):
     try:
-        with open(dir, mode='w', newline='') as archivo:
+        with open(dir, mode='w', newline='', encoding='utf-8') as archivo:
             data_csv = csv.DictWriter(archivo, fieldnames=fieldnames)
             data_csv.writeheader()
             data_csv.writerows(data)
@@ -47,6 +47,46 @@ def crear_productos():
     # usar la funcion para crear el archivo csv
     guardar_archivo_csv(DIR_PRODUCTOS, productos, FIELDNAMES_PRODUCTOS)
 
+def listar_productos():
+    os.system("cls")
+    print("== LISTADO DE PRODUCTOS ==")
+
+    productos = leer_archivo_csv(DIR_PRODUCTOS)
+    
+    headers = FIELDNAMES_PRODUCTOS
+    col_widths = [15,25,15,15]
+    
+    # Imprimir el encabezado
+    header_line = ' | '.join(f"{header.capitalize():^{col_widths[i]}}" for i, header in enumerate(headers))
+    print(header_line)
+    print('-' * len(header_line))
+    
+    for producto in productos:
+        row = ' | '.join(f"{str(producto.get(header, '')):^{col_widths[i]}}" for i, header in enumerate(headers))
+        print(row)
+
+def actualizar_productos():
+    os.system("cls")
+    print("== EDITAR PRODUCTO ==")
+    productos = leer_archivo_csv(DIR_PRODUCTOS)
+    
+    listar_productos()
+    id_producto = input("Ingrese el ID del producto que desea Editar >> ")   
+
+    nombre_producto = input("Ingrese el NOMBRE del producto >> ")
+    precio = int(input("Ingrese el VALOR del producto >> ")) # validar los int (try)
+    stock = int(input("Ingrese el STOCK del producto >> ")) # validar los int (try)
+
+    
+    for producto in productos:
+        if producto['id_producto'] == id_producto:
+            producto['nombre_producto'] = nombre_producto
+            producto['precio'] = precio
+            producto['stock'] = stock
+            break
+    
+    # usar la funcion para crear el archivo csv
+    guardar_archivo_csv(DIR_PRODUCTOS, productos, FIELDNAMES_PRODUCTOS)
 
 def menu_general():
     os.system("cls")
@@ -77,9 +117,9 @@ def iniciar_programa():
         if opcion == 1:
             crear_productos()
         elif opcion == 2:
-            print("Actualizar")
+            actualizar_productos()
         elif opcion == 3:
-            print("Listar")
+            listar_productos()
         elif opcion == 4:
             print("Borrar")
         elif opcion == 5:
